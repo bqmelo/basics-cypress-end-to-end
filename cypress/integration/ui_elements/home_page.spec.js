6d082311aebe1b0f94820b118419a826bf4cdcbd
Bruna Quinhones de Melo <bruna.melo@townsq.com.br>
2020-03-28 15:35:56 -0300

    Adding new test

--- cypress/integration/ui_elements/home_page.spec.js
+++ cypress/integration/ui_elements/home_page.spec.js
@@ -6,26 +6,44 @@ describe('Home Page', () => {
             cy.visit('/');
         });
 
-        it('Should have default elements', () => {
+        it('Should have contain default elements', () => {
             default_navigation_elements();
         });
 
-        it('Should have home page elements', () => {
+        it('Should have contain home page elements', () => {
             cy.get('#columns').children()
                 .should(($) => {
                     expect($).have.lengthOf(2);
-                    expect($[0]).to.be.visible;
+                    expect($).to.be.visible;
                     expect($[0]).to.have.attr('id', 'slider_row');
-                    expect($[1]).to.be.visible;
                     expect($[1]).to.have.attr('class', 'row');
                 });
-            cy.get('#homeslider').children({force: true})
+            cy.get('#homeslider').children()
                 .should(($) => {
                     expect($).have.lengthOf(5);
                     expect($[0]).to.be.visible;
                     expect($[0]).to.have.attr('style');
                 });
-            
+            cy.get('#htmlcontent_top > ul').children()
+                .should(($) => {
+                    expect($).have.lengthOf(2);
+                    expect($).to.be.visible;
+                });
+            cy.get('#center_column').children()
+                .should(($) => {
+                    expect($).have.lengthOf(3);
+                    expect($).to.be.visible;
+                });
+            cy.get('.homefeatured').should('have.attr', 'href', '#homefeatured');
+            cy.get('.blockbestsellers').should('have.attr', 'href', '#blockbestsellers');
+            cy.get('#homefeatured').children()
+                .should(($) => {
+                    expect($).to.be.visible;
+                });
+            cy.get('#blockbestsellers').children()
+                .should(($) => {
+                    expect($).to.be.hidden;
+                });
         });
     });
 });
--- cypress/integration/ui_elements/signin_page.spec.js
+++ cypress/integration/ui_elements/signin_page.spec.js
@@ -1,11 +1,38 @@
+import {default_navigation_elements} from './default_elements';
+
 describe('Sign in Page', () => {
     context('Default Elements', () => {
         before('Access site', () => {
             cy.visit('?controller=authentication&back=my-account');
         });
 
-        it('Should have second div with logo, search and cart', () => {
-            
+        it('Should have contain default elements', () => {
+            default_navigation_elements();
+        });
+
+        it('Should have contain authentication elements', () => {
+            cy.get('.home').should('have.attr', 'href', 'http://automationpractice.com/');
+            cy.get('.navigation_page').should('contain', '	Authentication');
+            cy.get('.page-heading').should('contain', 'Authentication');
+        });
+
+        it('Should have contain create account elements', () => {
+            cy.get('#create-account_form')
+                .should(($) => {
+                    expect($).to.have.attr('action', 'http://automationpractice.com/index.php?controller=authentication');
+                    expect($).to.have.attr('method', 'post');
+                });
+            cy.get('.navigation_page').should('contain', '	Authentication');
+            cy.get('.page-heading').should('contain', 'Authentication');
+            cy.get('.page-subheading').should('contain', 'Create an account');
+            cy.contains('Please enter your email address to create an account.').should('exist');
+            cy.get('#create_account_error').should('have.attr', 'style', 'display:none');
+            cy.get('.form-group').children()
+                .should(($) => {
+                    expect($[0]).to.contain('Email address');
+                    expect($[1]).to.have.attr('type', 'text');
+                });
+
         });
     });
 });
